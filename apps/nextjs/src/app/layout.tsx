8f28ab6edb4be0067fecb758ddb87c9a81e3142f
fillip1984 <fillip1984@gmail.com>
2024-03-10 20:43:37 -0400

    layed out ui, about to try something though

--- apps/nextjs/src/app/layout.tsx
+++ apps/nextjs/src/app/layout.tsx
@@ -1,43 +1,31 @@
-import type { Metadata, Viewport } from "next";
+import type { Metadata } from "next";
 import { GeistMono } from "geist/font/mono";
 import { GeistSans } from "geist/font/sans";
 
 import { cn } from "@acme/ui";
-import { ThemeProvider, ThemeToggle } from "@acme/ui/theme";
+import { ThemeProvider } from "@acme/ui/theme";
 import { Toaster } from "@acme/ui/toast";
 
-import { env } from "~/env";
 import { TRPCReactProvider } from "~/trpc/react";
 
 import "~/app/globals.css";
 
 export const metadata: Metadata = {
-  metadataBase: new URL(
-    env.VERCEL_ENV === "production"
-      ? "https://turbo.t3.gg"
-      : "http://localhost:3000",
-  ),
-  title: "Create T3 Turbo",
-  description: "Simple monorepo with shared backend for web & mobile apps",
-  openGraph: {
-    title: "Create T3 Turbo",
-    description: "Simple monorepo with shared backend for web & mobile apps",
-    url: "https://create-t3-turbo.vercel.app",
-    siteName: "Create T3 Turbo",
-  },
-  twitter: {
-    card: "summary_large_image",
-    site: "@jullerino",
-    creator: "@jullerino",
-  },
+  // metadataBase: new URL(
+  //   env.VERCEL_ENV === "production"
+  //     ? "https://turbo.t3.gg"
+  //     : "http://localhost:3000",
+  // ),
+  title: "locutus",
+  description: "Audiobook player",
 };
 
-export const viewport: Viewport = {
-  themeColor: [
-    { media: "(prefers-color-scheme: light)", color: "white" },
-    { media: "(prefers-color-scheme: dark)", color: "black" },
-  ],
-};
+// export const viewport: Viewport = {
+//   themeColor: [
+//     { media: "(prefers-color-scheme: light)", color: "white" },
+//     { media: "(prefers-color-scheme: dark)", color: "black" },
+//   ],
+// };
 
 export default function RootLayout(props: { children: React.ReactNode }) {
   return (
@@ -47,13 +35,12 @@ export default function RootLayout(props: { children: React.ReactNode }) {
           "min-h-screen bg-background font-sans text-foreground antialiased",
           GeistSans.variable,
           GeistMono.variable,
-        )}
-      >
+        )}>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           <TRPCReactProvider>{props.children}</TRPCReactProvider>
-          <div className="absolute bottom-4 right-4">
+          {/* <div className="absolute bottom-4 right-4">
             <ThemeToggle />
-          </div>
+          </div> */}
           <Toaster />
         </ThemeProvider>
       </body>
--- apps/nextjs/src/app/page.tsx
+++ apps/nextjs/src/app/page.tsx
@@ -1,40 +1,49 @@
-import { Suspense } from "react";
+import { CiBookmark } from "react-icons/ci";
+import { FaPause, FaPlay, FaStepBackward, FaStepForward } from "react-icons/fa";
+import { FaArrowRotateLeft, FaArrowRotateRight } from "react-icons/fa6";
 
-import { api } from "~/trpc/server";
-import { AuthShowcase } from "./_components/auth-showcase";
-import {
-  CreatePostForm,
-  PostCardSkeleton,
-  PostList,
-} from "./_components/posts";
-
-// export const runtime = "edge";
-
-export default async function HomePage() {
-  // You can await this here if you don't want to show Suspense fallback below
-  const posts = api.post.all();
-
+export default function HomePage() {
   return (
-    <main className="container h-screen py-16">
-      <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
-          Create <span className="text-primary">T3</span> Turbo
-        </h1>
-        <AuthShowcase />
+    <main className="flex items-center justify-center bg-slate-200 p-2">
+      <div className="mx-4 flex w-full flex-col rounded rounded-b-lg bg-slate-900 text-black">
+        <div className="flex items-center gap-3 p-6">
+          <div className="flex h-20 w-20 items-center justify-center rounded-lg bg-stone-300">
+            FSR
+          </div>
+          <div>
+            <p className="text-sm font-semibold text-sky-300">Ep. 128</p>
+            <p className="text-sm font-bold text-stone-400">
+              Scaling CSS at Heroku with Utility Classes
+            </p>
+            <p className="font-extrabold text-stone-200">Full Stack Radio</p>
+          </div>
+        </div>
+        <div className="mx-4 mb-6 flex flex-col gap-2">
+          <div className="relative h-2 rounded-xl bg-slate-300/30">
+            <div className="relative z-10 h-2 w-[50%] rounded-xl bg-sky-300">
+              <div className="absolute -right-1 -top-1 z-10 h-4 w-4 rounded-full border-4 border-white bg-sky-300"></div>
+            </div>
+          </div>
 
-        <CreatePostForm />
-        <div className="w-full max-w-2xl overflow-y-scroll">
-          <Suspense
-            fallback={
-              <div className="flex w-full flex-col gap-4">
-                <PostCardSkeleton />
-                <PostCardSkeleton />
-                <PostCardSkeleton />
-              </div>
-            }
-          >
-            <PostList posts={posts} />
-          </Suspense>
+          <div className="flex justify-between">
+            <p className="text-sm text-white">24:15</p>
+            <p className="text-sm text-white/40">75:50</p>
+          </div>
+        </div>
+        <div className="flex items-center justify-evenly rounded-b-lg bg-slate-300 p-4 text-slate-500">
+          <CiBookmark />
+          <FaStepBackward />
+          <FaArrowRotateLeft />
+          <div className="w-20"></div>
+          <div className="absolute flex h-20 w-20 flex-none items-center justify-center rounded-full border border-slate-300 bg-white text-xl">
+            <FaPause />
+            {/* <FaPlay /> */}
+          </div>
+          <FaArrowRotateRight />
+          <FaStepForward />
+          <div className="flex items-center justify-center rounded-lg border-2 border-slate-500 px-1 text-xs">
+            1x
+          </div>
         </div>
       </div>
     </main>
