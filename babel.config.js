8b4f8439f362ac3d588f12433522f51c4f10ee3b
fillip1984 <fillip1984@gmail.com>
2024-04-08 22:58:58 -0400

    removed dotenv since I'm no longer using it

--- babel.config.js
+++ babel.config.js
@@ -5,17 +5,6 @@ module.exports = function (api) {
       ["babel-preset-expo", { jsxImportSource: "nativewind" }],
       "nativewind/babel",
     ],
-    plugins: [
-      // See: https://dev.to/bhatvikrant/how-to-add-environment-variables-in-a-react-native-project-with-ts-2ne5
-      // TODO: switch to this library if our .env file ends up getting any more complicationed: https://github.com/t3-oss/t3-env
-      [
-        "module:react-native-dotenv",
-        {
-          moduleName: "@env",
-          path: ".env",
-        },
-      ],
-      ["inline-import", { extensions: [".sql"] }],
-    ],
+    plugins: [["inline-import", { extensions: [".sql"] }]],
   };
 };
