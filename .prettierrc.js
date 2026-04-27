9541d070c26a34b81c080eba244f2acdf29b08b6
fillip1984 <fillip1984@gmail.com>
2026-04-26 20:01:43 -0400

    still tweaking prettier

--- .prettierrc.js
+++ .prettierrc.js
@@ -3,10 +3,39 @@
 
 /** @type { PrettierConfig | TailwindConfig } */
 const config = {
-    plugins: [
-        "prettier-plugin-tailwindcss",
-    ],
-    bracketSameLine: true
+  plugins: [
+    "@ianvs/prettier-plugin-sort-imports",
+    "prettier-plugin-tailwindcss",
+  ],
+  importOrder: [
+    "<TYPES>",
+    "^(react/(.*)$)|^(react$)|^(react-native(.*)$)",
+    "^(next/(.*)$)|^(next$)",
+    "^(expo(.*)$)|^(expo$)",
+    "",
+    "<THIRD_PARTY_MODULES>",
+    "",
+    "<TYPES>^[.|..|~]",
+    "^~/",
+    "^[../]",
+    "^[./]",
+  ],
+  importOrderParserPlugins: ["typescript", "jsx", "decorators-legacy"],
+  importOrderTypeScriptVersion: "5.0.0",
+  overrides: [
+    {
+      files: "*.json.hbs",
+      options: {
+        parser: "json",
+      },
+    },
+    {
+      files: "*.ts.hbs",
+      options: {
+        parser: "babel",
+      },
+    },
+  ],
 };
 
 module.exports = config;
